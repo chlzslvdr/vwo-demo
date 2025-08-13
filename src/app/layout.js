af685d5cbf1e7d507804026d4a42a60a37830cf8
chlzslvdr <chelzeasalvador@gmail.com>
2025-08-13 20:59:35 +0800

    chore(vmo): fix vmo accountId

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,33 @@
 import './globals.css';
-import VWOScript from '../components/VWOScript';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <VWOScript />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var _vwo_code=(function(){
+                var account_id=${process.env.NEXT_PUBLIC_VWO_ACCOUNT_ID},
+                settings_tolerance=2000,
+                library_tolerance=2500,
+                use_existing_jquery=false,
+                is_spa=1;
+                return {
+                  use_existing_jquery:use_existing_jquery,
+                  library_tolerance:library_tolerance,
+                  settings_tolerance:settings_tolerance,
+                  hide_element:'body',
+                  /* DO NOT EDIT BELOW THIS LINE */
+                  f:false,d:document,
+                  // rest of VWO's code...
+                };
+              }());
+            `
+          }}
+        />
       </head>
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
