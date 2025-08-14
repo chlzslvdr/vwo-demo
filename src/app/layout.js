a40820b88836173fd8cf21c09688b38dee1476cc
chlzslvdr <chelzeasalvador@gmail.com>
2025-08-14 20:12:59 +0800

    refactor(vmo): use vwo-smartcode-nextjs

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,40 +1,16 @@
+import { VWOScript } from "vwo-smartcode-nextjs";
+
+export const metadata = {
+  title: "VWO Demo",
+  description: "A/B testing with VWO on Next.js App Router",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* VWO SmartCode directly in the server-rendered head */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              var _vwo_code=(function(){
-                var account_id=1130496,
-                settings_tolerance=2000,
-                library_tolerance=2500,
-                use_existing_jquery=false,
-                is_spa=1;
-                return {
-                  use_existing_jquery:use_existing_jquery,
-                  library_tolerance:library_tolerance,
-                  settings_tolerance:settings_tolerance,
-                  hide_element:'body',
-                  /* DO NOT EDIT BELOW THIS LINE */
-                  f:false,d:document,
-                  t: function() {
-                    var f=this;
-                    function d() {
-                      var a=document.createElement('script');
-                      a.src='https://dev.visualwebsiteoptimizer.com/j.php?a='+account_id+'&u='+encodeURIComponent(document.URL)+'&r='+Math.random();
-                      a.type='text/javascript';a.innerText;a.onerror=function(){};
-                      document.getElementsByTagName('head')[0].appendChild(a);
-                    }
-                    setTimeout(d, 0);
-                  }
-                };
-              }());
-              _vwo_code.t();
-            `
-          }}
-        />
+        {/* Add SmartCode inline in SSR */}
+        <VWOScript accountId={process.env.NEXT_PUBLIC_VWO_ACCOUNT_ID} />
       </head>
       <body>{children}</body>
     </html>
